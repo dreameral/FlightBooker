64713a10f1e291862e74b64d40451f2557cbf963
Ennio Sinanaj <enniosinanaj96@gmail.com>
2022-08-06 16:29:20 +0200

    Initial commit

---
+++ flightbooker-ms/src/main/java/com/flightbooker/flightbookerms/FlightbookerMsApplication.java
@@ -0,0 +1,13 @@
+package com.flightbooker.flightbookerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightbookerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightbookerMsApplication.class, args);
+    }
+
+}
---
+++ flightbooker-ms/src/test/java/com/flightbooker/flightbookerms/FlightbookerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightbooker.flightbookerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightbookerMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
