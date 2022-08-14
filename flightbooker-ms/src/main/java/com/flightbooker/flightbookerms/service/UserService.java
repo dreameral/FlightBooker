f726947a729f91202337ef8b285f47495887f558
Ennio Sinanaj <enniosinanaj96@gmail.com>
2022-08-14 10:44:40 +0200

    Add spring security to intercept requests and read the logged in username.

--- flightbooker-ms/src/main/java/com/flightbooker/flightbookerms/service/UserService.java
+++ flightbooker-ms/src/main/java/com/flightbooker/flightbookerms/service/UserService.java
@@ -1,20 +1,27 @@
 package com.flightbooker.flightbookerms.service;
 
+import com.flightbooker.flightbookerms.model.FlightBookerUserDetails;
 import com.flightbooker.flightbookerms.model.entity.User;
 import com.flightbooker.flightbookerms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
     public List<User> getAll() {
-        return userRepository.findAll();
+        return repository.findAll();
+    }
+
+    public Optional<FlightBookerUserDetails> getUserDetails(String username) {
+        Optional<User> user = repository.findByUsername(username);
+        return user.map(FlightBookerUserDetails::new);
     }
 
 }
